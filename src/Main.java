921968ced4873bec2e3f4ed5a794ab8e31baf858
Привет <mer79@internet.ru>
2022-08-01 20:40:16 +0300

    Домашшнее задание №3 часть 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        byte b = 5;
+        System.out.println(b);
+        int i = 25;
+        System.out.println(i);
+        short s = 1;
+        System.out.println(s);
+        long l = 1000l;
+        System.out.println(l);
+        float f = 125.6f;
+        System.out.println(f);
+        double d = 25.785;
+        System.out.println(d);
+        var v = 25;
+        System.out.println(v);
+
     }
+
+
 }
